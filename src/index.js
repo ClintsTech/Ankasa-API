aeb02d05451a998e0f5d6d2869cef526e0f5e368
Saidina Hamzah <hamzaahsaid@gmail.com>
2020-11-28 01:31:43 +0700

    CRUD Destination Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const authRoutes = require('./routes/auth')
+const destinationRoutes = require('./routes/destination')
 
 router.use('/auth', authRoutes)
+router.use('/destination', destinationRoutes)
 
 module.exports = router
