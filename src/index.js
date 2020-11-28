5386ec5b38655faa851337bfbfd8653ca2a2bbc7
Saidina Hamzah <hamzaahsaid@gmail.com>
2020-11-28 13:53:20 +0700

    CRUD airline

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ const authRoutes = require("./routes/auth");
 const destinationRoutes = require("./routes/destination");
 const userRoutes = require("./routes/user");
 const flightRoutes = require("./routes/flight");
+const airlineRoutes = require("./routes/airline");
 
 router.use("/auth", authRoutes);
 router.use("/destination", destinationRoutes);
 router.use("/user", userRoutes);
-
 router.use("/flight", flightRoutes);
+router.use("/airline", airlineRoutes);
 
 module.exports = router;
