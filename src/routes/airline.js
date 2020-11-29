32d5fd939de7be0306d605bf4af4c5b15e4b8c69
Saidina Hamzah <hamzaahsaid@gmail.com>
2020-11-29 13:32:50 +0700

    -

--- src/routes/airline.js
+++ src/routes/airline.js
@@ -2,8 +2,8 @@ const router = require('express').Router()
 const airlineController = require('../controllers/airline')
 
 router
-    .get('', airlineController.getAirline) //get all airline
-    .post('', airlineController.createAirline) //Create airline
-    .patch('', airlineController.updateAirline) //Edit airline
-    .delete('', airlineController.deleteAirline) //Delete airline
+    .get('/', airlineController.getAirline) //get all airline
+    .post('/', airlineController.createAirline) //Create airline
+    .patch('/', airlineController.updateAirline) //Edit airline
+    .delete('/', airlineController.deleteAirline) //Delete airline
 module.exports = router
