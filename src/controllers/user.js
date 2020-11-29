c471433b3cf55f04630db25535913a1d2a16fea3
amyusup <amyusup26@gmail.com>
2020-11-30 01:15:15 +0700

    update backend users new

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,35 +1,70 @@
-const response = require("../helpers/response");
 const userModel = require("../models/user");
+const { response } = require("../helpers");
+const bcrypt = require("bcrypt");
 
 module.exports = {
-  getAll: (req, res) => {
-    userModel
-      .getAll()
-      .then((data) => response.success(data, res))
-      .catch((err) => response.failed(err, res));
+  getAllUser: async function(req, res) {
+    try{
+      const { id } = req.token
+      const result = await userModel.getAllUser(id)
+      if (result[0]) {
+        response(res, 200, result);
+      } else {
+        response(res, 400, { message: "Data not avaliable" });
+      }
+    }catch(e){
+      response(res, 500, { message: "Failed Get all user" });
+    }
   },
-  getUser: (req, res) => {
-    userModel
-      .getUser(req.token)
-      .then((data) => response.success(data, res))
-      .catch((err) => response.failed(err, res));
+  getUserById: async function (req, res) {
+    try{
+      const { id } = req.token
+      const result = await userModel.getUserById(id)
+      const address = result[0].address.split(',')
+      result[0].city = address[0]
+      result[0].country = address[1]
+      delete result[0].password
+
+      // console.log(result[0].city )
+      // console.log(result)
+      if (result[0]) {
+        response(res, 200, result);
+      } else {
+        response(res, 400, { message: "User not found" });
+      }
+    }catch(e){
+      response(res, 500, { message: "Failed get data user" });
+    }
   },
-  setUser: (req, res) => {
-    userModel
-      .setUser(req.body)
-      .then((data) => response.success(data, res, "insert data success"))
-      .catch((err) => response.failed(err, res));
-  },
-  updateUser: (req, res) => {
-    userModel
-      .updateUser(req.token.role, req.body)
-      .then((data) => response.success(data, res, "Update data success"))
-      .catch((err) => response.failed(err, res));
-  },
-  terminateUser: (req, res) => {
-    userModel
-      .terminateUser(req.params.id)
-      .then((data) => response.success(data, res))
-      .catch((err) => response.failed(err, res));
+
+  updateUser: async function(req, res) {
+    try{
+      const { id } = req.token
+      const setData = req.body
+
+      if(req.file){
+        setData.photo = req.file.filename
+      }
+
+      if(setData.currPassword && setData.password){
+        const result = await userModel.getUserById(id)
+        const check = bcrypt.compareSync(setData.currPassword, result[0].password)
+        if(check){
+          const salt = bcrypt.genSaltSync(10)
+          const hash = bcrypt.hashSync(setData.password, salt)
+          setData.password = hash
+          delete setData.currPassword
+        }else{
+          response(res, 400, { message: "Failed update password" });
+        }
+      }
+
+      const result = await userModel.updateUser(id, setData)
+      if(result.affectedRows){
+        response(res, 201, result);
+      }
+    }catch(e){
+      response(res, 500, { message: "Internal server error" });
+    }
   },
 };
--- src/models/user.js
+++ src/models/user.js
@@ -2,81 +2,35 @@ const db = require("../config/mysql");
 const bcrypt = require("bcrypt");
 
 module.exports = {
-  getAll: () => {
+  getAllUser: function (id) {
     return new Promise((resolve, reject) => {
-      db.query("SELECT * FROM users ORDER BY id DESC", (err, res) => {
+      db.query(`SELECT * FROM users WHERE id !=${id}`, (err, result) => {
         if (!err) {
-          resolve(res);
+          resolve(result);
         }
-        reject(err);
+        reject(new Error(err));
       });
     });
   },
-  getUser: (token) => {
+  getUserById: function (id) {
     return new Promise((resolve, reject) => {
-      db.query(`SELECT * FROM users WHERE ?`, token.id, (err, res) => {
+      db.query(`SELECT * FROM users WHERE id=${id}`, (err, result) => {
         if (!err) {
-          resolve(res);
+          resolve(result);
         }
-        reject(err);
+        reject(new Error(err));
       });
     });
   },
-  setUser: (body) => {
+  
+  updateUser: function (id, setData) {
     return new Promise((resolve, reject) => {
-      const { password } = body;
-      bcrypt.hash(password, 10, function (err, hashPass) {
-        const newBody = { ...body, password: hashPass };
-        if (err) {
-          reject(err);
-        }
-        db.query(`INSERT INTO users SET ?`, newBody, (err, data) => {
-          if (!err) {
-            resolve(newBody);
-          } else {
-            reject(err);
-          }
-        });
-      });
-    });
-  },
-  updateUser: (id, body) => {
-    return new Promise((resolve, reject) => {
-      db.query(`SELECT * FROM users WHERE ?`, id, (err, result) => {
-        // console.log(result);
-        if (err) {
-          reject(err);
+      db.query(`UPDATE users SET ? WHERE id=${id}`, setData, (err, result) => {
+        if (!err) {
+          resolve(result);
         } else {
-          body.photo = !body.photo ? result[0].photo : body.photo;
-          let password = undefined;
-          if (!body.password) {
-            password = result[0].password;
-          } else {
-            password = bcrypt.hashSync(body.password, bcrypt.genSaltSync(10));
-          }
-          const newBody = { ...body, password: password };
-          db.query(
-            `UPDATE users SET ? WHERE id=${id}`,
-            newBody,
-            (err, data) => {
-              if (!err) {
-                resolve(newBody);
-              } else {
-                reject(err);
-              }
-            }
-          );
+          reject(new Error(err));
         }
-      });
-    });
-  },
-  terminateUser: (id) => {
-    return new Promise((resolve, reject) => {
-      db.query(`DELETE FROM users WHERE id=${id}`, (err, res) => {
-        if (!err) {
-          resolve(res);
-        }
-        reject(err);
       });
     });
   },
