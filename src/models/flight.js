b05c8afa9dfa80e9beb9f78ec4a86f0db7427345
Saidina Hamzah <hamzaahsaid@gmail.com>
2020-11-30 20:51:02 +0700

    fixing searchFlight

--- src/models/flight.js
+++ src/models/flight.js
@@ -6,7 +6,7 @@ module.exports = {
     const { city_departure, city_arrived, departure, classs , seat} = setData;
     return new Promise((resolve, reject) => {
       db.query(
-        `SELECT f.id flight_id, f.city_departure, f.city_arrived, f.price, a.name plane, a.seat, a.image logo, a.facilities, a.class, a.code, a.terminal, f.seat reserved_seats, f.departure, f.status, f.time_estimate, f.gate FROM flights f LEFT JOIN airlines a ON (f.plane = a.id) WHERE (f.city_departure = '${city_departure}' AND f.city_arrived = '${city_arrived}' AND f.seat >= ${seat} AND DATE(f.departure) = '${departure}' AND a.class='${classs}') LIMIT ${limit} OFFSET ${offset}`,
+        `SELECT f.id flight_id, f.city_departure, f.city_arrived, f.price, a.name plane, a.seat, a.image logo, a.facilities, a.class, a.code, a.terminal, f.seat reserved_seats, f.departure, f.status, f.time_estimate, f.gate FROM flights f LEFT JOIN airlines a ON (f.plane = a.id) WHERE (f.city_departure = '${city_departure}' AND f.city_arrived = '${city_arrived}' AND (a.seat - f.seat) >= ${seat} AND DATE(f.departure) = '${departure}' AND a.class='${classs}') LIMIT ${limit} OFFSET ${offset}`,
         (err, result) => {
           if (!err) {
             resolve(result);
