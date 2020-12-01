6ea175536ca490ab14dcde70dedb48d5857c3f72
amyusup <amyusup26@gmail.com>
2020-12-01 18:53:19 +0700

    fix routes

--- src/index.js
+++ src/index.js
@@ -6,11 +6,8 @@ const flightRoutes = require("./routes/flight");
 const airlineRoutes = require("./routes/airline");
 const bookingRoutes = require("./routes/booking");
 const reviewRoutes = require("./routes/review");
-<<<<<<< HEAD
 const chatRoutes = require("./routes/chat");
-=======
 const paymentRoutes = require("./routes/payment");
->>>>>>> bb2cac507a9973252576623ac67af531b4d42fe6
 
 router.use("/auth", authRoutes);
 router.use("/destination", destinationRoutes);
@@ -19,10 +16,7 @@ router.use("/flight", flightRoutes);
 router.use("/airline", airlineRoutes);
 router.use("/booking", bookingRoutes);
 router.use("/review", reviewRoutes);
-<<<<<<< HEAD
 router.use("/chat", chatRoutes);
-=======
 router.use("/payment", paymentRoutes);
->>>>>>> bb2cac507a9973252576623ac67af531b4d42fe6
 
 module.exports = router;
